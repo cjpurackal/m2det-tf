a5ca29f4793acb9ff896b0b482938cbaefed57f4
anandhupvr <anandhupvr@gmail.com>
2019-05-19 19:37:42 +0530

    pep8, removing unwanted import

--- m2det/classifier.py
+++ m2det/classifier.py
@@ -4,19 +4,19 @@ from tensorflow.keras.activations import softmax
 
 
 def simple_classifier(config, feature):
-    num_classes = 2 + 1 # background
-    num_anchors = config["anchors"]["num_anchors"]
-    cls = Conv2D(
-        filters=num_classes*num_anchors,
-        kernel_size=(3, 3),strides=(1, 1),
-        padding='same',use_bias=True
-        )(feature)
-    cls = BatchNormalization()(cls)
-    sh = feature.shape
-    cls = softmax(
-        tf.reshape(
-            cls,
-            [-1, sh[1] * sh[2] * num_anchors, num_classes]
-            )
-        )
-    return cls
+	num_classes = 2 + 1  # background
+	num_anchors = config["anchors"]["num_anchors"]
+	cls = Conv2D(
+		filters=num_classes*num_anchors,
+		kernel_size=(3, 3), strides=(1, 1),
+		padding='same', use_bias=True
+		)(feature)
+	cls = BatchNormalization()(cls)
+	sh = feature.shape
+	cls = softmax(
+		tf.reshape(
+			cls,
+			[-1, sh[1] * sh[2] * num_anchors, num_classes]
+			)
+		)
+	return cls
